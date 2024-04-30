3a3c83bd7dbd7bf099cb5361668106a740e6e40a
priscilla goessens <priscillagoessens@gmail.com>
2024-04-30 09:00:02 +0200

    remove react.strictMode for one render of profile network

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //Provider permet de rendre le store disponible a toute mon app
 root.render(
-  <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
-  </React.StrictMode>,
 );
